4a06722effb110372e180c0b5368617f2b96fbbb
williyamshoe <william.sheu@berkeley.edu>
2018-02-05 22:40:46 -0800

    completed proj1b

--- proj1b/PalindromeFinder.java
+++ proj1b/PalindromeFinder.java
@@ -1,9 +1,5 @@
 /** This class outputs all palindromes in the words file in the current directory. */
 public class PalindromeFinder {
-<<<<<<< HEAD
-=======
-    /*
->>>>>>> 4dc3688acd57f7b4d456e67c298094f62a0629e7
     public static void main(String[] args) {
         int minLength = 4;
         In in = new In("../library-sp18/data/words.txt");
@@ -15,9 +11,5 @@ public class PalindromeFinder {
                 System.out.println(word);
             }
         }
-<<<<<<< HEAD
     }
-=======
-    } Uncomment this class once you've written isPalindrome. */
->>>>>>> 4dc3688acd57f7b4d456e67c298094f62a0629e7
 }
