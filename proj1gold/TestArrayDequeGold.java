c95986a9bee17768b87db56a0da0adb28f0ba284
williyamshoe <william.sheu@berkeley.edu>
2018-02-06 14:49:51 -0800

    completed proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -5,22 +5,22 @@ public class TestArrayDequeGold {
     public void test1 () {
         StudentArrayDeque a = new StudentArrayDeque();
         ArrayDequeSolution b = new ArrayDequeSolution();
-        String message = "sh ";
+        String message = "";
         while (true) {
             double random = StdRandom.uniform();
             if (random < 0.25 || (b.isEmpty() && random < 0.5)) {
                 a.addFirst((int) (random * 200));
                 b.addFirst((int) (random * 200));
-                message = message + "addFront("+ ((int) (random * 200)) +") ";
+                message = message + "addFront("+ ((int) (random * 200)) +")\n";
             } else if (random < 0.5 || b.isEmpty()) {
                 a.addLast((int) (random * 100));
                 b.addLast((int) (random * 100));
-                message = message + "addLast("+ ((int) (random * 100)) +") ";
+                message = message + "addLast("+ ((int) (random * 100)) +")\n";
             } else if (random < 0.75) {
-                message = message + "removeFirst() ";
+                message = message + "removeFirst()\n";
                 assertEquals(message, b.removeFirst(), a.removeFirst());
             } else {
-                message = message + "removeFirst() ";
+                message = message + "removeFirst()\n";
                 assertEquals(message, b.removeLast(), a.removeLast());
             }
         }
