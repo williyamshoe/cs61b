d9f394c1c2508a6dfc20cb3ff6ae7eea61ac51f1
williyamshoe <william.sheu@berkeley.edu>
2018-04-05 16:30:55 -0700

    completed lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +13,54 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int target;
+    private int source;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        source = maze.xyTo1D(sourceX, sourceY);
+        target = maze.xyTo1D(targetX, targetY);
+        distTo[source] = 0;
+        edgeTo[source] = source;
+    }
+
+    private class Node{
+        int loc;
+        int prev;
+        int dist;
+        Node(int l, int p, int d) {
+            loc = l;
+            prev = p;
+            dist = d;
+        }
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Node current = new Node(-1, -1, -1);
+        ArrayDeque<Node> fringe = new ArrayDeque<>();
+        fringe.addLast(new Node(source, source, 0));
+        marked[source] = true;
+        while (current.loc != target) {
+            current = fringe.removeFirst();
+            edgeTo[current.loc] = current.prev;
+            marked[current.loc] = true;
+            distTo[current.loc] = current.dist;
+            for (int i : maze.adj(current.loc)){
+                if (!marked[i]) {
+                    fringe.addLast(new Node(i, current.loc, current.dist + 1));
+                }
+            }
+            announce();
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +11,59 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int source;
+    private Maze maze;
+    private boolean cycleFound = false;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        source = maze.xyTo1D(1, 1);
+        distTo[source] = 0;
+        edgeTo[source] = source;
     }
+
+    private class Node{
+        int loc;
+        int prev;
+        int dist;
+        Node(int l, int p, int d) {
+            loc = l;
+            prev = p;
+            dist = d;
+        }
+    }
+
+
+    private void cyclefinder() {
+        Node current;
+        ArrayDeque<Node> fringe = new ArrayDeque<>();
+        fringe.addLast(new Node(source, source, 0));
+        marked[source] = true;
+        while (!cycleFound) {
+            current = fringe.removeFirst();
+            edgeTo[current.loc] = current.prev;
+            marked[current.loc] = true;
+            distTo[current.loc] = current.dist;
+            for (int i : maze.adj(current.loc)){
+                if (marked[i] && current.prev != i) {
+                    edgeTo[i] = current.loc;
+                    cycleFound = true;
+                }else if (current.prev != i) {
+                    fringe.addLast(new Node(i, current.loc, current.dist + 1));
+                }
+            }
+            announce();
+            if (fringe.isEmpty()){
+                return;
+            }
+        }
+    }
+
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        cyclefinder();
     }
-
-    // Helper methods go here
 }
 
